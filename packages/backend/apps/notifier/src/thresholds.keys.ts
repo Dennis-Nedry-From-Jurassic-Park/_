a3c3ba60b7e298a8e2625a93881a63ab19816cf6
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-02-23 19:43:56 +0300

    structs

--- packages/backend/apps/notifier/src/thresholds.keys.ts
+++ packages/backend/apps/notifier/src/thresholds.keys.ts
@@ -1,5 +1,11 @@
 export const NONE = "[]";
 
+export const Source = {
+    coingecko: 'coingecko',
+    imoex: 'imoex',
+    spbe_hkd: 'spbe_hkd',
+}
+
 export const ThresholdKey = {
     COINGECKO: 'tresholds.crypto.coingecko',
     IMOEX: 'tresholds.shares.ti.imoex',
@@ -10,6 +16,10 @@ export const getVal = (obj, key) => {
     return obj[key.toUpperCase()];
 }
 
+export const ThresholdDir = {
+    above: 'above',
+    below: 'below',
+}
 export const ThresholdType = {
     NONE: 'none',
     COINGECKO: 'crypto.coingecko',
@@ -17,5 +27,13 @@ export const ThresholdType = {
     SPBE_HKD: 'shares.spbe_hkd',
 };
 
+export type NotifierMap = Map<string, {
+    price: number,
+    thresholds: number[],
+    oldIndex: number,
+    newIndex: number,
+}>
+
+export type Source = typeof Source[keyof typeof Source]
 export type ThresholdKey = typeof ThresholdKey[keyof typeof ThresholdKey]
 export type ThresholdType = typeof ThresholdType[keyof typeof ThresholdType]
