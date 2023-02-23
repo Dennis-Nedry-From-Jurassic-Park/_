4cdff79f83573944b476558cc69ea697e92c3f73
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-02-23 19:43:36 +0300

    dto

--- packages/backend/apps/notifier/src/thresholds.dto.ts
+++ packages/backend/apps/notifier/src/thresholds.dto.ts
@@ -1,9 +1,16 @@
-export type TickerValue = {
-    ticker: string;
-    values: number[];
+export type Alert = {
+    dir: string,
+    val: number,
+}
+export type TickerAlert = {
+    ticker: string,
+    alerts: Alert[],
 }
 
 export type Threshold = {
     type: string,
-    data: Array<TickerValue>
+    data: Array<TickerAlert>
 }
+
+export type TickerPrice = { "ticker": string, "price": number }
+export type TickerThreshold = { "ticker": string, "thresholds": number[] }
