2b7608b08f41c63c02c3639a69cb90f1654a1aa4
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-02-23 19:45:54 +0300

    algo#2: rm hashmap (dir above|below for alerts (thresholds) price)

--- packages/backend/apps/notifier/src/app.notifier.controller.ts
+++ packages/backend/apps/notifier/src/app.notifier.controller.ts
@@ -1,7 +1,7 @@
 import {Controller, Header, Post, Req} from "@nestjs/common";
 import {ThresholdService} from "./app.service";
-import {TickerPrice, TickerAlert} from "./thresholds.dto";
-import {NotifierMap, Source, ThresholdDir} from "./thresholds.keys";
+import {TickerAlert, TickerPrice} from "./thresholds.dto";
+import {Source, ThresholdDir} from "./thresholds.keys";
 
 const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
@@ -12,7 +12,7 @@ export class NotifierController {
 
     @Post([
         "crypto/coingecko"
-    ]) // TODO: по крону запуск раз в 60 секунд
+    ]) // TODO: cron + tg message
     @Header('content-type', 'application/json')
     async notifier_crypto(
         @Req() req: Request
@@ -52,38 +52,5 @@ export class NotifierController {
             }
             console.log(`ticker: ${ticker} = ${price}`);
         }
-
-        //
-        //
-        //
-        // if (this.appService.b_notifier_crypto_coingecko) {
-        //     this.appService.notifier_crypto_coingecko = await this
-        //         .appService
-        //         .update_thresholds(_thresholds, prices, this.appService.b_notifier_crypto_coingecko)
-        // } else {
-        //     this.appService.notifier_crypto_coingecko = await this
-        //         .appService
-        //         .update_thresholds(_thresholds, prices, this.appService.b_notifier_crypto_coingecko)
-        //     this.appService.b_notifier_crypto_coingecko = true;
-        // }
-        //
-        // console.log(this.appService.notifier_crypto_coingecko);
-        //
-        // for(let [ticker, value] of this.appService.notifier_crypto_coingecko) {
-        //     const oldIndex = value.oldIndex
-        //     const newIndex = value.newIndex
-        //     console.log(`текущая цена ${ticker} ` + value.price);
-        //
-        //     if(oldIndex != newIndex) {
-        //         const price = value.price
-        //
-        //         console.log(`цена ${ticker} достигла уровня ` + price);
-        //
-        //         this.appService.notifier_crypto_coingecko.get(ticker)!.oldIndex = -1
-        //         this.appService.notifier_crypto_coingecko.get(ticker)!.newIndex = -1
-        //
-        //         // TODO: send to telegram notification
-        //     }
-        // }
     }
 }
